d77f36b1dd1049b89002adf64d4834878ee8a880
musikele <michele.nasti@gmail.com>
2016-12-10 16:14:03 +0100

    migliorato script di compilazione

--- compile.sh
+++ compile.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+SOURCE_BRANCH="develop"
+
+git clone $REPO compile
+cd compile 
+git checkout $SOURCE_BRANCH || git checkout --orphan $SOURCE_BRANCH
+
 bundle install
 bundle exec jekyll build
+
+cp ./_site/* ../out/*
