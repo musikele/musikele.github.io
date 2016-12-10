cfbde5ee1e49f357bbea9cdb1e89630763e3beb2
musikele <michele.nasti@gmail.com>
2016-12-10 16:23:57 +0100

    copia ricorsiva

--- compile.sh
+++ compile.sh
@@ -11,6 +11,6 @@ git checkout $SOURCE_BRANCH || git checkout --orphan $SOURCE_BRANCH
 bundle install
 bundle exec jekyll build
 
-cp ./_site/**/* out/
+cp -r ./_site/ out/
 
 echo "finished compiling!"
