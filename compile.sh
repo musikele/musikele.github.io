0c466ce7b915ad760292499607bbe2121f919dac
musikele <michele.nasti@gmail.com>
2017-01-02 15:52:17 +0100

    aggiunta timezone per  forestry.io

--- compile.sh
+++ compile.sh
@@ -12,7 +12,7 @@ cd compile
 git checkout $SOURCE_BRANCH || git checkout --orphan $SOURCE_BRANCH
 
 bundle install
-bundle exec jekyll build --future
+bundle exec jekyll build
 cd _site/
 cp -r ./* ../../out/
 cd ..
